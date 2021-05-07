b6b130fa96cafe1c2f33cf3c0c93c7a7676ea4ea
Regis Sales <regis@prodadospesquisa.com.br>
2021-05-07 15:52:44 -0300

    model task

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
